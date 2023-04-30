8dca388285010f20c2edb217defca2c4799d9aae
Tri Pham <trcpham@ucsc.edu>
2023-04-29 22:09:09 -0700

    Update script.js

--- silly story/script.js
+++ silly story/script.js
@@ -16,7 +16,7 @@ randomize.addEventListener('click', result);
 
 function result() {
 
-    const newStory = storyText;
+    var newStory = storyText;
 
     const xItem = randomValueFromArray(insertX);
     const yItem = randomValueFromArray(insertY);  
