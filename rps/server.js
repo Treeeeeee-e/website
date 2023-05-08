033dd252d1889ef1ed8c01ebd3cfa10d1cca71a8
Tri Pham <trcpham@ucsc.edu>
2023-05-08 15:43:34 -0700

    Update server.js

--- rps/server.js
+++ rps/server.js
@@ -7,6 +7,20 @@ app.get('/', (req, res) => {
     res.send('Hello World! Welcome to Node.js')
 })
 
+//Get function  that returns the number of visist for node sessions
+app.get('/visits', (req, res) => { //at the '/visits' route
+	counter += 1 //increase our counter variable by 1
+      // Text formatting from ‘text formatting’ JavaScript prelab section
+	res.send(`There have been ${counter} visits to this session`);
+});
+
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
 })
+
+
+
+
+
+
